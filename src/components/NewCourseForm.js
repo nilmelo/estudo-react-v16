991b6b292b1eeab1d08d97f3a5562fc39e66a024
Nilson Melo <nilson.melo@sti.ufc.br>
2023-09-21 13:54:47 -0300

    feature: api initial requests

--- src/components/NewCourseForm.js
+++ src/components/NewCourseForm.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 class NewCourseForm extends Component {
 
     static defaultProps = {
+        categories: [],
         onsubmit: () => {}
     };
 
@@ -56,11 +57,11 @@ class NewCourseForm extends Component {
                     <span>Categoria:</span>
                     <select name="category" onChange={this.handleChange} value={state.category}>
                         <option value="">Selecionar</option>
-                        <option value="Javascript">Javascript</option>
-                        <option value="PHP">PHP</option>
-                        <option value="Ruby">Ruby</option>
-                        <option value="Python">Python</option>
-                        <option value="C#">C#</option>
+                        {
+                            props.categories.map(category => (
+                                <option value={category.name}>{category.name}</option>
+                            ))
+                        }
                     </select>
                 </label>
                 <button type="submit">Criar Curso</button>
